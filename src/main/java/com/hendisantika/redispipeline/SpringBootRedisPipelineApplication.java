bdace4835d2f37ef671744972fe36aac31faf6c8
hendisantika <hendisantika@yahoo.co.id>
2023-12-20 07:50:16 +0700

    Create starter project 🫸🌀✏️📗 :octocat:🐧🐳

---
+++ src/main/java/com/hendisantika/redispipeline/SpringBootRedisPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.redispipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRedisPipelineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRedisPipelineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/redispipeline/SpringBootRedisPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.redispipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRedisPipelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
